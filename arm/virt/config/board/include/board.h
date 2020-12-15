4e6c8f652c5931c656cce544172f358f588645dc
Zbigniew Bodek <zbigniew.bodek@huawei.com>
2020-12-15 14:00:10 +0000

    Clean-up address space, increase RAM size to 1GB
    Clean-up memory map to use majority of platform's
    registers.
    
    Since the kernel should now be able to handle
    Qemu ARM Virt's RAM base and size, increase RAM size
    to 1GB.
    
    Signed-off-by: Zbigniew Bodek <zbigniew.bodek@huawei.com>
    Change-Id: Ib7627e7dadfcad9765a95f790a2478106caa84c4

--- arm/virt/config/board/include/board.h
+++ arm/virt/config/board/include/board.h
@@ -26,17 +26,40 @@ extern "C" {
 
 /* physical memory base and size */
 #define DDR_MEM_ADDR            0x40000000
-#define DDR_MEM_SIZE            0x04000000
+#define DDR_MEM_SIZE            0x40000000
+/**
+ * Memory map is as follows:
+ * ADDR         SIZE
+ * 0x00000000 ; 0x08000000	Flash
+ * 0x08000000 ; 0x01000000	GIC (all variants' registers)
+ * 0x09000000 ; 0x00001000	UART
+ * 0x09010000 ; 0x00001000	RTC
+ * 0x09020000 ; 0x00000018	FW Config
+ * 0x09030000 ; 0x00001000	GPIO
+ * 0x09040000 ; 0x00001000	Secure UART
+ * 0x09050000 ; 0x00020000	SMMU
+ * 0x09070000 ; 0x00000018	PCDIMM ACPI
+ * 0x09080000 ; 0x00000004	ACPI GED
+ * 0x09090000 ; 0x00000004	NVDIMM ACPI
+ * 0x0a000000 ; 0x00000200	MMIO
+ * 0x0c000000 ; 0x02000000	Platform Bus
+ * 0x0e000000 ; 0x01000000	Secure Mem
+ * 0x10000000 ; 0x2eff0000	PCIe MMIO
+ * 0x3eff0000 ; 0x00010000	PCIe PIO
+ * 0x3f000000 ; 0x01000000	PCIe ECAM
+ * 0x40000000 ; 0xXXXXXXXX	RAM
+ */
 
 /* Peripheral register address base and size */
-#define PERIPH_PMM_BASE         0x08000000
-#define PERIPH_PMM_SIZE         0x02000000
+#define PERIPH_PMM_BASE         0x00000000
+/* Note: Size ranges to the end of Secure Mem */
+#define PERIPH_PMM_SIZE         0x0F000000
 
 #define KERNEL_VADDR_BASE       0x40000000
 #define KERNEL_VADDR_SIZE       DDR_MEM_SIZE
 
 #define SYS_MEM_BASE            DDR_MEM_ADDR
-#define SYS_MEM_SIZE_DEFAULT    0x2000000
+#define SYS_MEM_SIZE_DEFAULT    0x02000000
 #define SYS_MEM_END             (SYS_MEM_BASE + SYS_MEM_SIZE_DEFAULT)
 
 #define EXC_INTERACT_MEM_SIZE        0x100000
