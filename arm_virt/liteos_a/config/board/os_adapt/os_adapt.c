94bbf015915e9138543bdd24862c571c9557eb36
SimonLi <likailong@huawei.com>
2021-07-17 16:19:31 +0800

    build(net): 删掉无用的ethnet_hieth_init，该函数会被链接依赖报错
    Signed-off-by: SimonLi <likailong@huawei.com>

--- arm_virt/liteos_a/config/board/os_adapt/os_adapt.c
+++ arm_virt/liteos_a/config/board/os_adapt/os_adapt.c
@@ -52,36 +52,7 @@
 void net_init(void)
 {
 extern void tcpip_init(tcpip_init_done_fn initfunc, void *arg);
-    dprintf("\ntcpip_init start\n");
     tcpip_init(NULL, NULL);
-    dprintf("\ntcpip_init end\n");
-
-    PRINTK("Ethernet start.");
-
-#ifdef LOSCFG_DRIVERS_HIGMAC
-    extern int ethnet_higmac_init(void);
-    (void)ethnet_higmac_init();
-#endif
-
-#ifdef LOSCFG_DRIVERS_HIETH_SF
-    extern int ethnet_hieth_init(void);
-    (void)ethnet_hieth_init();
-    struct netif *pnetif = NULL;
-
-    extern void get_defaultNetif(struct netif **pnetif);
-    get_defaultNetif(&pnetif);
-
-    (void)netifapi_netif_set_up(pnetif);
-    unsigned int overtime = 0;
-    do {
-        LOS_Msleep(SLEEP_TIME_MS);
-        overtime++;
-        if (overtime > NETIF_SETUP_OVERTIME) {
-            PRINTK("netif_is_link_up overtime!\n");
-            break;
-        }
-    } while (netif_is_link_up(pnetif) == 0);
-#endif
 
     extern struct netif *VirtnetInit(void);
     struct netif *p = VirtnetInit();
