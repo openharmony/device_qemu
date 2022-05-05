42237726040c65700ac886ff73e2ea2f4e80b968
xiexg-dc <xiexg@digitalchina.com>
2022-05-05 17:22:18 +0800

    feat:在riscv32_virt中新增littlefs适配
    在riscv32_virt中新增littlefs适配，并支持littlefs自动生成文件系统镜像。
    
    fix #I55WN9
    
    Signed-off-by: xiexg-dc <xiexg@digitalchina.com>
    Change-Id: I7524189b8e7971a7be5cba404a86d8d755ae7744

--- riscv32_virt/liteos_m/board/driver/cfiflash/cfiflash.h
+++ riscv32_virt/liteos_m/board/driver/cfiflash/cfiflash.h
@@ -15,7 +15,7 @@
 #ifndef __CFIFLASH_H__
 #define __CFIFLASH_H__
 
-#include "ff_gen_drv.h"
+#include "stdint.h"
 
 #define CFIFLASH_MAX_NUM            2
 #define CFIFLASH_CAPACITY           (32 * 1024 * 1024)
@@ -23,6 +23,31 @@
 #define CFIFLASH_ERASEBLK_WORDS     (CFIFLASH_ERASEBLK_SIZE / sizeof(uint32_t))
 #define CFIFLASH_ERASEBLK_WORDMASK  (~(CFIFLASH_ERASEBLK_WORDS - 1))
 
-DiskioDrvTypeDef *GetCfiBlkOps(void);
+/* Results of Flash Functions */
+typedef enum {
+    FLASH_OK = 0,     /* 0: Successful */
+    FLASH_ERROR       /* 1: R/W Error */
+} FLASH_DRESULT;
 
-#endif
+unsigned CfiFlashSec2Bytes(unsigned sector);
+
+int CfiFlashQuery(uint32_t pdrv);
+int32_t CfiFlashRead(uint32_t pdrv, uint32_t *buffer, uint32_t offset, uint32_t nbytes);
+int32_t CfiFlashWrite(uint32_t pdrv, const uint32_t *buffer, uint32_t offset, uint32_t nbytes);
+int32_t CfiFlashErase(uint32_t pdrv, uint32_t offset);
+
+#if (LOSCFG_SUPPORT_FATFS == 1)
+#include "fatfs.h"
+#include "ff_gen_drv.h"
+
+DiskioDrvTypeDef *GetCfiBlkOps(void);
+#endif /* LOSCFG_SUPPORT_FATFS == 1 */
+
+#if (LOSCFG_SUPPORT_LITTLEFS == 1)
+#include "lfs.h"
+
+int LittlefsDriverInit(void);
+struct lfs_config* GetCfiLfsCfg(void);
+#endif /* LOSCFG_SUPPORT_LITTLEFS == 1 */
+
+#endif /* __CFIFLASH_H__ */
